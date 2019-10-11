7a21b8d40993838d67394d1e2c401cd315216bff
AitovRR <r_aitov@mail.ru>
2019-10-11 16:23:24 +0300

    Урок 3

---
+++ Урок3/example1.py
@@ -0,0 +1,17 @@
+# Типы данных
+# str - строки char - символ
+# Строка это набор char'ов: 'a'
+# 'Hello, world!'
+# '' - это пустая строка
+# int - целые числа
+# 1, 2, -50, 100
+# float - действительные числа
+# 5.0, 2.3, -1,5
+# bool - логические значения
+# True False
+
+# Пример 1
+a = input()
+# print(a + 1)  # Error
+print(int(a) + 1)  # str to int
+print(a + str(1))  # int to str
